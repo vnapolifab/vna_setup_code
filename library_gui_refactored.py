df74cebd1d7daf773cef2a1b953fb022980f6ae1
Andrea Micelli <61918542+andrea-mic@users.noreply.github.com>
2024-05-08 03:18:17 +0200

    Update to library gui refactored

--- library_gui_refactored.py
+++ library_gui_refactored.py
@@ -3,6 +3,7 @@ import numpy as np
 import ast
 import json
 from abc import ABC, abstractmethod
+from icecream import ic
 
 import tkinter as tk
 from tkinter import ttk, messagebox
@@ -38,29 +39,38 @@ class GUI:
         self.entries = entries
         self.buttons = buttons
 
-        for i, entry in enumerate(self.entries.values()):
-            entry.setup(self, i)
+        row = 0
+        for entry in self.entries:
+            entry.setup(row)
+            entry.row = row
+            row += entry.rows_occupied
 
-        for j, button in enumerate(self.buttons.values()):
-            button.setup(self, i+j+1)
+        for button in self.buttons:
+            button.setup(row)
+            row += entry.rows_occupied
         
         self.root.mainloop()
         self.root.quit()
+
+
+    def find_entry(self, param_name):
+        for entry in self.entries:
+            if entry.param_name == param_name:
+                return entry
+        raise Exception(f"Parameter {param_name} is not associated with an GUI_Input object")
         
 
     def clear_all(self):
-        for name, entry in zip(self.entries.keys(), self.entries.values()):
+        for entry in self.entries:
             entry.clear()
     
 
     def get_values(self):
         self.inputs = {}
-        for name, entry in zip(self.entries.keys(), self.entries.values()):
-            if entry.entry_type == GUI_input.BUTTON: 
-                continue
+        for entry in self.entries:
 
             if not(entry.is_valid()):
-                tk.messagebox.showerror(title=None, message=f'Input for "{entry.param_desc.lower()}" is not valid!')
+                tk.messagebox.showerror(title=None, message=f'Input for "{entry.param_desc}" is not valid!')
                 return
             
             self.inputs[entry.param_name] = entry.get()
@@ -71,59 +81,59 @@ class GUI:
     def load(self, filename):
         with open(filename, "r") as f:
             json_obj = json.load(f)
-        for param, val  in zip(json_obj.keys(), json_obj.values()):
-            if param in self.entries.keys():
-                self.entries[param].write(val)
+        for entry in self.entries:
+            entry.write(json_obj.get(entry.param_name, ""))
 
 
 
 
 class GUI_input(ABC):
+    rows_occupied = 1
     TEXT = 1
     COMBOBOX = 2
-    BUTTON = 3
 
-    def __init__(self, gui, param_name, param_desc):
+    def __init__(self, gui, param_name, param_desc, mandatory = True, hidden = False):
         self.gui = gui
         self.param_name = param_name
         self.param_desc = param_desc
+        self.mandatory = mandatory
+        self.hidden = hidden
 
     @abstractmethod
     def setup(self):
         pass
 
     @abstractmethod
-    def is_valid():
+    def is_valid(self):
         pass
 
     @abstractmethod
-    def clear():
+    def clear(self):
         pass
 
     @abstractmethod
-    def write():
+    def write(self):
         pass
 
     @abstractmethod
-    def get():
+    def get(self):
         pass
 
 
 
+# ====================== TEXT ======================
 
-class GUI_input_standard_text(GUI_input):
+
+class GUI_input_text(GUI_input):
     entry_type = GUI_input.TEXT
 
-    def __init__(self, gui, param_name, param_desc):
-        super().__init__(gui, param_name, param_desc)
-
-    def setup(self, gui, row):
-        ttk.Label(gui.root, text=self.param_desc, background='light grey').grid(row=row, column=0, sticky="w", padx=5, pady=5)
-        self.entry_var = ttk.Entry(gui.root, width=50)
+    def setup(self, row):
+        ttk.Label(self.gui.root, text=self.param_desc, background='light grey').grid(row=row, column=0, sticky="w", padx=5, pady=5)
+        self.entry_var = ttk.Entry(self.gui.root, width=50)
         self.entry_var.grid(row=row, column=1, sticky="ew", padx=5, pady=5)
     
     def is_valid(self):
-        if self.entry_var.get() != "":
+        if self.entry_var.get() != "" or self.mandatory == False:
             return True
         else:
             return False
@@ -142,16 +152,20 @@ class GUI_input_standard_text(GUI_input):
         return self.entry_var.get()
 
 
+
+# ====================== COMBOBOX ======================
+
+
 class GUI_input_combobox(GUI_input):
     entry_type = GUI_input.COMBOBOX
 
-    def __init__(self, gui, param_name, param_desc, values):
+    def __init__(self, values, **kwargs):
         self.values = values
-        super().__init__(gui, param_name, param_desc)
+        super().__init__(**kwargs)
 
-    def setup(self, gui, row):
-        ttk.Label(gui.root, text=self.param_name, background='light grey').grid(row=row, column=0, sticky="w", padx=5, pady=5)
-        self.entry_var = ttk.Combobox(gui.root, state="readonly", values=self.values, width=50)
+    def setup(self, row):
+        ttk.Label(self.gui.root, text=self.param_desc, background='light grey').grid(row=row, column=0, sticky="w", padx=5, pady=5)
+        self.entry_var = ttk.Combobox(self.gui.root, state="readonly", values=self.values, width=50)
         self.entry_var.grid(row=row, column=1, sticky="ew", padx=5, pady=5)
         self.entry_var.bind('<<ComboboxSelected>>', self.on_change)
 
@@ -167,22 +181,66 @@ class GUI_input_combobox(GUI_input):
     def write(self, content):
         self.entry_var.set(content)
 
-    def on_change(self, event):
-        print("Changed value")
-
     def get(self):
         return self.entry_var.get()
 
+    def on_change(self, event):
+        pass
 
+
+class GUI_input_combobox_user_name(GUI_input_combobox):
+    rows_occupied = 2
+    NEW_USER = "---New User---"
+
+    def setup(self, *args, **kwargs):
+        self.entry_var_text = ttk.Entry(self.gui.root, width=50)
+        super().setup(*args, **kwargs)
+
+    def get(self):
+        combobox_input = super().get()
+        return combobox_input if combobox_input != self.NEW_USER else self.entry_var_text.get()
+
+    def on_change(self, event):
+        if self.entry_var.get() == self.NEW_USER:
+            self.entry_var_text.grid(row=self.row+1, column=1, sticky="ew", padx=5, pady=5)
+            self.gui.find_entry("sample_name").entry_var["values"] = [GUI_input_combobox_sample_name.NEW_SAMPLE]
+        else:
+            self.entry_var_text.grid_remove()
+            self.gui.find_entry("sample_name").entry_var["values"] = [GUI_input_combobox_sample_name.NEW_SAMPLE] + find_subfolder( os.path.join(DATA_FOLDER_NAME, self.get()) )
+
+
+class GUI_input_combobox_sample_name(GUI_input_combobox):
+    rows_occupied = 2
+    NEW_SAMPLE = "---New Sample---"
+
+    def setup(self, *args, **kwargs):
+        self.entry_var_text = ttk.Entry(self.gui.root, width=50)
+        super().setup(*args, **kwargs)
+
+    def get(self):
+        combobox_input = super().get()
+        return combobox_input if combobox_input != self.NEW_SAMPLE else self.entry_var_text.get()
+
+    def on_change(self, event):
+        if self.entry_var.get() == self.NEW_SAMPLE:
+            self.entry_var_text.grid(row=self.row+1, column=1, sticky="ew", padx=5, pady=5)
+        else:
+            self.entry_var_text.grid_remove()
+            
+
+
+# ====================== BUTTONS ======================
 
 
 class GUI_button(ABC):
+    rows_occupied = 1
+
     def __init__(self, gui, button_name):
         self.gui = gui
         self.button_name = button_name
 
-    def setup(self, gui, row):
-        self.entry_var = ttk.Button(gui.root, text=self.button_name, command=self.on_press, width=20)
+    def setup(self, row):
+        self.entry_var = ttk.Button(self.gui.root, text=self.button_name, command=self.on_press, width=20)
         self.entry_var.grid(row=row, column=1, columnspan=1, padx=10, pady=10)
 
     @abstractmethod
@@ -191,79 +249,58 @@ class GUI_button(ABC):
 
 
 class GUI_button_submit(GUI_button):
-    def __init__(self, gui, button_name):
-        super().__init__(gui, button_name)
-
     def on_press(self):
-        gui.get_values()
+        self.gui.get_values()
 
 
 class GUI_button_clear(GUI_button):
-    entry_type = GUI_input.BUTTON
-
-    def __init__(self, gui, button_name):
-        super().__init__(gui, button_name)
-
     def on_press(self):
-        gui.clear_all()
+        self.gui.clear_all()
 
 
 class GUI_button_load_last_settings(GUI_button):
-    entry_type = GUI_input.BUTTON
-
-    def __init__(self, gui, button_name):
-        super().__init__(gui, button_name)
-
     def on_press(self):
-        gui.load("last_settings.json")
+        self.gui.load("last_settings.json")
 
 
+
+def find_subfolder(folder_path):
+    try:
+        subfolders = next(os.walk(folder_path))[1]
+    except FileNotFoundError:
+        messagebox.showerror("Folder Not Found", f"The '{folder_path}' folder does not exist.")
+        subfolders = []
+    return subfolders
 
 
 
 def gui_startup():
-    global gui
     gui = GUI(root=tk.Tk())
 
-    entries = {
-        
-        "user_name" : GUI_input_combobox(gui=gui, param_name="user_name", param_desc="User", values=[1,2,3]),
-        
-        "sample_name" : GUI_input_combobox(gui=gui, param_name="sample_name", param_desc="Sample", values=[1,2,3]),
-        
-        "measurement_name" : GUI_input_standard_text(gui=gui, param_name="measurement_name", param_desc="Measurement name"),
-
-        "description" : GUI_input_standard_text(gui=gui, param_name="description", param_desc="Description"),
-
-        "dipole_mode" : GUI_input_combobox(gui=gui, param_name="dipole_mode", param_desc="Dipole mode", values=[1,2,3]),
-
-        "s_parameter" : GUI_input_standard_text(gui=gui, param_name="s_parameter", param_desc="Field sweep [mT]"),
-
-        "field_sweep" : GUI_input_standard_text(gui=gui, param_name="field_sweep", param_desc="Field sweep [mT]"),
-
-        "angle" : GUI_input_standard_text(gui=gui, param_name="angle", param_desc="Angle [deg]"),
-
-        "start_frequency" : GUI_input_standard_text(gui=gui, param_name="start_frequency", param_desc="Start frequency [GHz]"),
-
-        "stop_frequency" : GUI_input_standard_text(gui=gui, param_name="stop_frequency", param_desc="Stop frequency [GHz]"),
-        
-        "number_of_points" : GUI_input_standard_text(gui=gui, param_name="number_of_points", param_desc="Number of points"),
-        
-        "bandwith" : GUI_input_standard_text(gui=gui, param_name="bandwidth", param_desc="Bandwidth [Hz]"),
-
-        "power" : GUI_input_standard_text(gui=gui, param_name="power", param_desc="Power [dBm]"),
-        
-        "ref_field" : GUI_input_standard_text(gui=gui, param_name="ref_field", param_desc="Ref field [mT]"),
-
-    }
+    entries = [
+        GUI_input_combobox_user_name(   gui=gui,    param_name="user_name",            param_desc="User",                  values=[GUI_input_combobox_user_name.NEW_USER] + find_subfolder(DATA_FOLDER_NAME)),
+        GUI_input_combobox_sample_name( gui=gui,    param_name="sample_name",          param_desc="Sample",                values=[]),
+        GUI_input_text(                 gui=gui,    param_name="measurement_name",     param_desc="Measurement name"       ),
+        GUI_input_text(                 gui=gui,    param_name="description",          param_desc="Description",           mandatory=False),
+        GUI_input_combobox(             gui=gui,    param_name="dipole_mode",          param_desc="Dipole mode",           values=[]),
+        GUI_input_text(                 gui=gui,    param_name="s_parameter",          param_desc="Field sweep [mT]"       ),
+        GUI_input_text(                 gui=gui,    param_name="field_sweep",          param_desc="Field sweep [mT]"       ),
+        GUI_input_text(                 gui=gui,    param_name="angle",                param_desc="Angle [deg]"            ),
+        GUI_input_text(                 gui=gui,    param_name="start_frequency",      param_desc="Start frequency [GHz]"  ),
+        GUI_input_text(                 gui=gui,    param_name="stop_frequency",       param_desc="Stop frequency [GHz]"   ),
+        GUI_input_text(                 gui=gui,    param_name="number_of_points",     param_desc="Number of points"       ),
+        GUI_input_text(                 gui=gui,    param_name="bandwidth",            param_desc="Bandwidth [Hz]"         ),
+        GUI_input_text(                 gui=gui,    param_name="power",                param_desc="Power [dBm]"            ),
+        GUI_input_text(                 gui=gui,    param_name="ref_field",            param_desc="Ref field [mT]"         ),
+    ]
 
 
-    buttons = {
-        "submit" : GUI_button_submit(gui=gui, button_name="Submit"),
-        "clear" : GUI_button_clear(gui=gui, button_name="Clear"),
-        "load"  : GUI_button_load_last_settings(gui=gui, button_name="Load last settings")
-    }
 
+    buttons = [
+        GUI_button_submit(              gui=gui,    button_name="Submit"            ),
+        GUI_button_clear(               gui=gui,    button_name="Clear"             ),
+        GUI_button_load_last_settings(  gui=gui,    button_name="Load last settings")
+    ]
 
 
     gui.run_gui(entries=entries, buttons=buttons)
