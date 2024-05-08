b6b5ec4ed77cdbb5fcf746189ae56efdf081c357
vnapolifab <vnapolifab@gmail.com>
2024-05-08 09:45:22 +0200

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,23 @@
+from library_analysis import *
+from library_gui import *
+from library_vna import *
+from library_power_supply import *
+from CONSTANTS import *
+
+
+# Connects power supplies
+print("Power supply 1 > ", end=""); 
+if "ps1" in locals(): ps1.closeConnection()
+ps1 = setupConnectionPS('COM4', 9600)
+
+print("Power supply 2 > ", end=""); 
+if "ps2" in locals(): ps2.closeConnection()
+ps2 = setupConnectionPS('COM3', 9600)
+
+# Connects VNA
+print("VNA            > ", end=""); 
+instr = setupConnectionVNA()
+print()
+
+
+ps2.setCurrent(2)
