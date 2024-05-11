1de8a318cae47ac1baac64ab6c1ec2dbc622f70f
Andrea Micelli <61918542+andrea-mic@users.noreply.github.com>
2024-05-11 02:32:29 +0200

    Update library_file_management.py

--- library_file_management.py
+++ library_file_management.py
@@ -118,7 +118,7 @@ def save_plot(path: str, name: str):
     folder_path = os.path.join(path, "Plots")
     if not(os.path.exists(folder_path)):
         os.mkdir(folder_path)
-    plt.savefig(os.path.join(folder_path, name))  # Might raise error if folder does not exist # TODO
+    plt.savefig(os.path.join(folder_path, name))
 
 
 if __name__ == "__main__":
