7b6c65036d56f727159d11f0ab0bafe746b6e7e4
vnapolifab <vnapolifab@gmail.com>
2024-10-14 16:37:38 +0200

    Update gui_measurement.py

--- gui_measurement.py
+++ gui_measurement.py
@@ -55,7 +55,7 @@ measurement_routine(
 
 # Save metadata:
 old_name = settings["measurement_name"]
-for sparam in ["S11", "S41", "S14", "S44"]: #TODO scommenta
+for sparam in ["S33", "S43", "S34", "S44"]: #TODO scommenta
     settings["measurement_name"] = f"{old_name}_{sparam}"
     settings["s_parameter"] = sparam
 update_log(settings)
