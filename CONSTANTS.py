e7c65ea690aa514d164f90a727f246b891c95ede
Albi99-Tony <albertotoniato@gmail.com>
2025-08-01 20:44:49 +0200

    Create CONSTANTS.py

---
+++ CONSTANTS.py
@@ -0,0 +1,7 @@
+AXIS_FONTSIZE = 16
+FULLSCREEN_SIZE = (13, 8)
+MARKER = "."
+DATA_FOLDER_NAME = r"C:\Users\rfprobestation\Desktop\DATA" #Folder where the data are located (absolute path)
+DEBUG_MODE = True
+MARKER_SIZE = 4
+SETTLING_TIME = 0.25
